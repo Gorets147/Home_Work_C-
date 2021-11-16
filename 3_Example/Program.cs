f5bc494731ffa7b1d15ad97e8c02ef107507ebc8
Михаил Комогорцев <mih.kom1998@gmail.com>
2021-11-16 20:52:35 +0300

    3_Example

---
+++ 3_Example/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Даны два числа. Показать большее и меньшее число
+
+Console.WriteLine("Введите 1-e число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-e число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number < number2)
+{
+    System.Console.WriteLine(number + " Меньшее число");
+    System.Console.WriteLine(number2 + " Большее число");
+}
+else
+{
+    System.Console.WriteLine(number + " Большее число");
+    System.Console.WriteLine(number2 + " Меньшее число");
+}
