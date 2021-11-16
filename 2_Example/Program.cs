5188da918bb7be2ce35e1ca32e0183e6f1e32600
Михаил Комогорцев <mih.kom1998@gmail.com>
2021-11-16 20:47:41 +0300

    2_Example

---
+++ 2_Example/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1. По двум введённым числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите 1-e число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-e число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number % number2 ==0)
+{
+    System.Console.WriteLine("1-e число является квадратом 2-го");
+}
+else
+{
+    System.Console.WriteLine("1-e число НЕявляется квадратом 2-го");
+}
