b4a4bb91d9969d5835c44aa6d9cfc712b8d1c2e6
Михаил Комогорцев <mih.kom1998@gmail.com>
2022-01-05 19:34:17 +0300

    9, 10 Examples added

---
+++ 10_Example/Program.cs
@@ -0,0 +1,7 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+
+System.Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine((number/10)%10);
+
---
+++ 9_Example/Program.cs
@@ -0,0 +1,5 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+
+int number = 123;
+
+System.Console.WriteLine(number%10);
