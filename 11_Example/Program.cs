860da88c5efe9f01c235827fddd4bad90bdba248
Михаил Комогорцев <mih.kom1998@gmail.com>
2022-01-05 20:17:14 +0300

    11_Example added

---
+++ 11_Example/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+
+System.Console.WriteLine("Введите двухзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void FindMax(int test_number)
+{
+    if (test_number <= 10 || test_number >= 99)
+    {
+        System.Console.WriteLine("Число введено не верно");
+    }
+    else
+    {
+        int a = test_number % 10;
+        int b = test_number / 10;
+        if (a < b) System.Console.WriteLine("Большее число: " + b);
+        else System.Console.WriteLine("Большее число: " + a);
+    }
+}
+
+FindMax(number);
