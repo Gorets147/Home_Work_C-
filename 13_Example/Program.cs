c63f57ad0b5489acb2f412e0679f03a7ec272ee3
Михаил Комогорцев <mih.kom1998@gmail.com>
2022-01-05 21:26:29 +0300

    13_Example added

---
+++ 13_Example/Program.cs
@@ -0,0 +1,21 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+System.Console.WriteLine("Введите делимое: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите делитель: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+void Del(int a, int b)
+{
+    if (a % b == 0)
+    {
+        System.Console.WriteLine($"Число {a}, кратно числу {b} и равно {a / b}");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {a}, НЕкратно числу {b} остаток: {a % b}");
+    }
+}
+
+Del(num1, num2);
